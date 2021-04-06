0cd2f59972872e153dd817fb81782d3bd8c1a993
Glitch (quaint-gray-ermine) <none>
2021-04-06 16:58:40 +0000

    😄👐 Checkpoint ./app.json:30631409/242 ./package.json:30631409/456 ./start.js:30631409/319

--- start.js
+++ start.js
@@ -2,14 +2,14 @@ const http = require("http");
 const { execSync } = require("child_process");
 
 const isThisGlitch = (
-    process.env.PROJECT_DOMAIN !== quaint-gray-ermine &&
+    process.env.PROJECT_DOMAIN !== undefined &&
     process.env.PROJECT_INVITE_TOKEN !== undefined &&
     process.env.API_SERVER_EXTERNAL !== undefined &&
     process.env.PROJECT_REMIX_CHAIN !== undefined);
 
 if (isThisGlitch) {
     http.createServer((req, res) => {
-        const now = new Date().toLocaleString("en-US");
+        const now = new Date().toLocaleString("es-MX");
         res.end(`OK (200) - ${now}`);
     }).listen(process.env.PORT);
 
@@ -24,7 +24,7 @@ function start(glitch = false) {
     if (glitch) {
         console.info("[INFO] Glitch environment detected, trying to compile...");
         execSync("npm run compile");
-        console.info("[INFO] Compiled, starting the bot...");
+        console.info("[INFO] Iniciando el bot...");
         if (process.env.CONFIG_CACHE_YOUTUBE_DOWNLOADS === "yes") console.warn("[WARN] Using cache on Glitch environment is not recommended, it will eat the project storage drastically.");
     }
     require("./dist/main.js");
